775deff380a06126944cf05387a0a01d2ea949e9
soyaOHNO <souya1683@gmail.com>
2026-01-25 16:31:23 +0900

    .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAHNOFleWLrNp2_308OekeRJCSNV3z5Gdg",
-  authDomain: "tsushimakendo-key-management.firebaseapp.com",
-  projectId: "tsushimakendo-key-management",
-  storageBucket: "tsushimakendo-key-management.firebasestorage.app",
-  messagingSenderId: "231389675503",
-  appId: "1:231389675503:web:b72e6432a409eb0e05001a",
-  measurementId: "G-MZFZ50P99R"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
